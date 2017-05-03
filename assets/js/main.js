8a65541e9945074306baf3bb4abeb3cf381d59be
aries0d0f <aries0d0f@gmail.com>
2017-05-03 12:03:16 +0800

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: '',
+        msgs: '',
+        count: {
+            dd: '',
+            hh: '',
+            mm: '',
+            ss: ''
+        },
+        end: ''
+    }
+})
+
+var content, distance
+
+function init() {
+    $.getJSON('./config.json', (data) => {
+        content = data
+    })
+}
+
+function time() {
+    app.end = new Date(content.end).getTime()
+    distance = app.end - new Date().getTime()
+    app.count.dd = Math.floor(distance / (1000 * 60 * 60 * 24))
+    app.count.hh = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    app.count.mm = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    app.count.ss = Math.floor((distance % (1000 * 60)) / 1000)
+}
+
+init()
+
+$(document).ready(function () {
+    app.title = content.title
+    app.msgs = content.msgs
+    setInterval("time()", 1000)
+})
