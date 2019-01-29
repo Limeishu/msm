64a02bff4e9a7922fb1dc07f5c16251ae4d841ee
Aries Cs <aries0d0f@gmail.com>
2019-01-29 22:38:59 +0800

    Deploy: ready for build to subdir

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/view/Home'
+import Landing from '@/view/Landing'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      redirect: '/2019/landing'
+    },
     {
       path: '/',
       name: 'Home',
@@ -14,6 +19,19 @@ export default new Router({
       meta: {
         label: '首頁'
       }
+    },
+    {
+      path: '/landing',
+      name: 'Landing',
+      component: Landing,
+      meta: {
+        label: 'Landing'
+      }
     }
-  ]
+  ].map(routeRule => {
+    routeRule.path = `/2019${routeRule.path}`
+    return routeRule
+  })
 })
+
+export default router
