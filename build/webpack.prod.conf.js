e2536446316cc22c63f0f262a15cb724c9b5f35d
Aries Cs <aries0d0f@gmail.com>
2019-01-29 23:19:55 +0800

    Deploy: add prerender package

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const PrerenderPlugin = require('prerender-spa-plugin')
 
 const env = require('../config/prod.env')
 
@@ -46,7 +47,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: utils.assetsPath('css/[name].[contenthash].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
       allChunks: true,
     }),
@@ -115,7 +116,18 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+
+    new PrerenderPlugin(
+      path.resolve(__dirname, '../dist'),
+      [ '/2019', '/2019/landing' ],
+      {
+        renderAfterTime: 5000,
+        phantomPageSettings: {
+          loadImages: true
+        }
+      }
+    )
   ]
 })
 
