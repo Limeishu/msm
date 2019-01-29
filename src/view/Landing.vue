57c44894252266f28e28c19b056607e857dc3a5f
Aries Cs <aries0d0f@gmail.com>
2019-01-29 23:20:07 +0800

    Layout: RWD

--- src/view/Landing.vue
+++ src/view/Landing.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <div
-      class="banner has-bg column-bottom"
+      class="banner has-bg column-center"
       :style="{'background-image': 'url(https://image.limeishu.org.tw/images/museum/cca200001-fa-oilms0110.jpg)'}"
     >
       <div class="landing-header">
         <div class="logo">
           <img src="@/assets/images/logo.svg" alt>
+        </div>
+        <div class="filter-box">
+          <h1>
+            2019 梅樹月 3/16～4/28 在三峽 <br />
+            邀您一同共襄盛舉
+          </h1>
         </div>
         <Counter :endDate="endDate" v-if="endDate.getTime() > new Date().getTime()"/>
       </div>
@@ -38,10 +44,43 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin-bottom: 12rem;
+    padding: 2rem;
+
+    .filter-box {
+      filter: drop-shadow(0 0 10px $white);
+    }
+
+    h1, p {
+      color: $white;
+      filter: drop-shadow(0 0 3px $black);
+      text-align: center;
+    }
+
+    h1 {
+      font-size: 24pt;
+      margin: 7pt 0;
+
+      @media screen and (max-width: 600px) {
+        font-size: 20pt;
+      }
+
+      @media screen and (max-width: 475px) {
+        font-size: 16pt;
+      }
+
+    }
 
     .logo {
       width: 40rem;
+
+      @media screen and (max-width: 840px) {
+        width: 80%;
+      }
+
+      @media screen and (max-width: 600px) {
+        width: 100%;
+      }
+
       filter: drop-shadow(0 0 10px $white);
       img {
         filter: drop-shadow(0 0 3px $black);
