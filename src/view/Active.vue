f39db1218156ff790da1251d8b73e6bf4689378f
Aries Cs <aries0d0f@gmail.com>
2019-03-10 17:09:34 +0800

    Layout: add active page

--- src/view/Active.vue
+++ src/view/Active.vue
@@ -1,7 +1,35 @@
 <template>
   <div
-    class="spotlight-container banner has-bg column-center"
+    class="spotlight-container page"
   >
-    <h1>活動</h1>
+    <div class="spotlight">
+      <div class="container">
+        <div
+          v-for="active in actives"
+          :key="active.title"
+          class="content"
+        >
+          <h1 class="serif">{{ active.title }}</h1>
+          <ul>
+            <li><p>講師：{{ active.speaker }}</p></li>
+            <li><p>時間：{{ active.time }}</p></li>
+            <li><p>地點：{{ active.place }}</p></li>
+            <li><p>備註：{{ active.note }}</p></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import activeData from '../../static/json/active.json'
+
+export default {
+  data () {
+    return {
+      actives: activeData
+    }
+  }
+}
+</script>
