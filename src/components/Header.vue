0bd058b09206858ac98568515784dec8073ef5d0
Aries Cs <aries0d0f@gmail.com>
2019-03-10 20:20:15 +0800

    Content: update footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,11 +20,11 @@
         <div class="year">2019</div>
         <div class="date">03/16-04/28</div>
         <div class="footer">
-          <p><span class="property">主辦單位｜</span><span>李梅樹紀念館（財團法人李梅樹文教基金會）</span></p>
-          <p><span class="property">合辦單位｜</span><span>國立臺北大學（圖書館，民俗藝術與文化資產研究所）</span></p>
-          <p><span class="property">指導單位｜</span><span>文化部，教育部，新北市政府文化局</span></p>
-          <p><span class="property">協辦單位｜</span><span>日本長野縣塩尻市本洗馬歷史里資料館，台灣創價學會（財團法人創價文教基金會），林育淳，林柏亭，長流美術館，長野澄子，財團法人陳進紀念文化藝術基金會，財團法人陳澄波文化基金會財團法人新北市三峽長福巖清水祖師公，國立臺灣圖書館，國史館臺灣文獻館，雄獅圖書股份有限公司，郭雪湖基金會，順益台灣原住民博物館，新北市三峽區成福國小，臺北市立美術館</span></p>
-          <p><span class="property">贊助單位｜</span><span>天美藝術基金會，康百力建設股份有限公司，富邦產物保險股份有限公司，新北市三峽區公所，新北市三峽區長福平面停車場，新北市三峽區龍恩里辦公處</span></p>
+          <p><span class="property">主辦單位</span><span>李梅樹紀念館（財團法人李梅樹文教基金會）</span></p>
+          <p><span class="property">合辦單位</span><span>國立臺北大學（圖書館，民俗藝術與文化資產研究所）</span></p>
+          <p><span class="property">指導單位</span><span>文化部，教育部，新北市政府文化局</span></p>
+          <p><span class="property">協辦單位</span><span>日本長野縣塩尻市本洗馬歷史里資料館，台灣創價學會（財團法人創價文教基金會），林育淳，林柏亭，長流美術館，長野澄子，財團法人陳進紀念文化藝術基金會，財團法人陳澄波文化基金會財團法人新北市三峽長福巖清水祖師公，國立臺灣圖書館，國史館臺灣文獻館，雄獅圖書股份有限公司，郭雪湖基金會，順益台灣原住民博物館，新北市三峽區成福國小，臺北市立美術館</span></p>
+          <p><span class="property">贊助單位</span><span>天美藝術基金會，康百力建設股份有限公司，富邦產物保險股份有限公司，新北市三峽區公所，新北市三峽區長福平面停車場，新北市三峽區龍恩里辦公處</span></p>
 
           <div class="copyright">
             <a class="footer-logo" href="https://limeishu.org.tw/" title="李梅樹紀念館官網" target="_blank" rel="noopener noreferrer">
