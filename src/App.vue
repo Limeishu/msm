248dae7318ac26b93b6d1619c39fe1583ea857e3
Aries Cs <aries0d0f@gmail.com>
2019-03-10 14:35:50 +0800

    Layout: main page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,43 @@
 <template>
   <div id="app">
-    <router-view />
+    <Header :class="{ 'toggle': isToggleHeader }" />
+    <transition :name="transistionMode">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </transition>
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  components: {
+    Header
+  },
+  data () {
+    return {
+      transitionMode: 'slide-left',
+      isToggleHeader: false
+    }
+  },
+  mounted () {
+    this.toggleHeader()
+  },
+  methods: {
+    toggleHeader () {
+      if (this.$route.name !== 'Home') this.isToggleHeader = true
+      else this.isToggleHeader = false
+    }
+  },
+  watch: {
+    $route () {
+      this.toggleHeader()
+    }
+  }
+}
+</script>
 
 <style lang="scss">
   @import "./assets/scss/main.scss";
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    id="header"
+    class="spotlight-container banner has-bg column-center"
+    :style="{'background-image': 'url(/2019/static/img/bg1-horizon.jpg)'}"
+  >
+    <div class="spotlight">
+      <div class="main-logo">
+        <img src="@/assets/images/logo.svg" alt="">
+      </div>
+      <div class="active-logo">
+        <img src="@/assets/images/active-logo.svg" alt="">
+        <div class="slogen">
+          <span class="serif">一場追尋歷史遺跡，重塑時代記憶的展覽</span>
+        </div>
+      </div>
+      <div class="active-date">
+        <div class="year">2019</div>
+        <div class="date">03/16-04/28</div>
+      </div>
+    </div>
+  </div>
+</template>
