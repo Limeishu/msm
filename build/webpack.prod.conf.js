e53d06146159fbeda5bb2711866d212e0766951b
Aries Cs <aries0d0f@gmail.com>
2019-03-12 01:12:24 +0800

    Deploy: modify prerender list

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -120,7 +120,7 @@ const webpackConfig = merge(baseWebpackConfig, {
 
     new PrerenderPlugin(
       path.resolve(__dirname, '../dist'),
-      [ '/2019', '/2019/landing', '/2019/active', '/2019/exhibition' ],
+      [ '/2019', '/2019/landing', '/2019/intro', '/2019/active', '/2019/exhibition' ],
       {
         renderAfterTime: 5000,
         phantomPageSettings: {
