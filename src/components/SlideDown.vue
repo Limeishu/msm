f5b4b82618a7614f75d94f82aa0b4a2275f73f90
Aries Cs <aries0d0f@gmail.com>
2019-03-11 20:22:05 +0800

    Layout: adjust RWD

--- src/components/SlideDown.vue
+++ src/components/SlideDown.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="slide-down">
+  <div
+    class="slide-down"
+    :class="{ 'home': $route.name === 'Home' }"
+  >
     <div class="hint">
       <p>捲動以瀏覽更多內容</p>
     </div>
