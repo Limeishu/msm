e91b26a5fb145f9745cdd16942466984c96cce74
Aries Cs <aries0d0f@gmail.com>
2019-03-12 00:56:00 +0800

    Layout: add intro page

---
+++ src/view/Intro.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="spotlight-container page"
+  >
+    <div class="spotlight">
+      <div class="container">
+        <h1 class="serif">展覽緣起</h1>
+        <div class="content">
+          <p>梅樹月邁入第八年，今年的主題是「韶光荏苒」，展覽內容自我定位為「一場追尋歷史足跡、重塑時代記憶的展覽」。</p>
+          <p>環繞於此主題之下，共有三個展點的三場系列展覽，分別是：在三峽祖師廟廟埕廣場的「鏗鏘歲月──三峽祖師廟重建影像紀事」、李梅樹紀念館的「百年對語──油彩與丹青的邂逅」、臺北大學草原上戶外美術館的「眾神的國度」。</p>
+          <p>參展對象除了李梅樹作品之外，還有日治時期東洋畫啟蒙導師鄉原古統，以及有「臺展三少年」之稱的林玉山、陳進、郭雪湖等前輩東洋畫家，和古蹟建築學者李乾朗等人作品；另外，也特別邀請三峽成福國小的小小藝術家們共同參與展覽活動。舉辦此一系列展覽，不是為了懷舊，而是希望藉由展覽內容透露的線索與訊息，重新追索前人走過的歷史足跡與時代記憶。失去歷史的記憶，如同是拔除土地的根源，也將抹滅人與土地的記憶和情感。</p>
+          <p>時間，讓作品更加滿溢著歷史的張力。藝術家創作的每一件作品，就好比一盞燈，在隱晦的時光隧道中，照映出時代的印記。李梅樹曾說：「富貴浮雲，藝術千秋。」期待透過梅樹月舉辦的系列展覽，以歷史串接土地的記憶，用藝術淬煉時代的雋永。</p>
+        </div>
+        <h1 class="serif">什麼是梅樹月？</h1>
+        <div class="content">
+          <h2 class="serif">如果以為梅樹月是來賞梅，可就大錯特錯了</h2>
+          <p>自 2012 年起，每年三至四月之間，由李梅樹紀念館主辦「梅樹月」藝術文化活動， 傳承與發揚李梅樹熱愛鄉土的精神，以藝術的平民化、生活化為理念，在地人文歷史的關注為本，結合跨領域的當代藝術家，希望讓藝術跨越美術館的藩籬，傳遞給人們更多的感動。</p>
+        </div>
+        <h1 class="serif">歷年梅樹月</h1>
+        <div class="content">
+          <h2 class="serif">這邊提供歷年梅樹月官網或手冊，供大家回味</h2>
+          <ul>
+            <li><p>2018 梅樹月網站<span><a href="https://msm.limeishu.org.tw/2018" target="_blank" rel="noopener noreferrer">立即前往</a></span></p></li>
+            <li><p>2017 梅樹月網站<span><a href="https://msm.limeishu.org.tw/2017" target="_blank" rel="noopener noreferrer">立即前往</a></span></p></li>
+            <li><p>2017 梅樹月手冊<span><a href="https://issuu.com/verybooks/docs/2017_limeishu-fest" target="_blank" rel="noopener noreferrer">立即查看</a></span></p></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
